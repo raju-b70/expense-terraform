d6801b4ee9d5c50c1bd165669ebfcb6045d97d74
rajendra <rajendrammarboyana@gmail.com>
2024-03-20 12:37:06 +0000

    terraform createa nd destroy

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "instance" {
   ami           = data.aws_ami.ami.image_id
-  instance_type = "var.instance_type"
+  instance_type = var.instance_type
   vpc_security_group_ids = [data.aws_security_group.selected.id]
   tags = {
     name = var.component
