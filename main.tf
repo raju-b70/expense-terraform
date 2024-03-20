4c1f8093dd61df6ce2a2ec60c70551e6b1274d66
rajendra <rajendrammarboyana@gmail.com>
2024-03-20 15:34:05 +0000

    terraform createa nd destroy

--- main.tf
+++ main.tf
@@ -8,10 +8,29 @@ module "frontend" {
   zone_id = var.zone_id
 }
 
+module "backend" {
+  depends_on = [module.mysql]
+  source = "./modules/app"
+  instance_type = var.instance_type
+  component = "backend"
+  ssh_user = var.ssh_user
+  ssh_password = var.ssh_password
+  env = var.env
+  zone_id = var.zone_id
+}
 
-#module "frontend" {
- #  source = "./modules/app"
-# }
-#module "frontend" {
-#  source = "./modules/app"
-#}
+module "mysql" {
+  source = "./modules/app"
+  instance_type = var.instance_type
+  component = "mysql"
+  ssh_user = var.ssh_user
+  ssh_password = var.ssh_password
+  env = var.env
+  zone_id = var.zone_id
+}
+
+
+
+
+
+
