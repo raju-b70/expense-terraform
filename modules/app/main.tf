c7f3248ee8afae7851e716c102bedc5faf68bf1d
rajendra <rajendrammarboyana@gmail.com>
2024-03-26 16:27:37 +0000

    adding policy to role

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "instance" {
+ resource "aws_instance" "instance" {
   ami                    = data.aws_ami.ami.image_id
   instance_type          = var.instance_type
   vpc_security_group_ids = [data.aws_security_group.selected.id]
